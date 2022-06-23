30c80a9115746e6f917fe6ed88e0aee7855cf9a5
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-23 23:08:46 +0430

    Add Color model

--- src/models/Color.java
+++ src/models/Color.java
@@ -1,2 +1,28 @@
-package models;public class Color {
+package models;
+
+public class Color {
+    private String name;
+    private String code;
+
+    public Color (String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
 }
