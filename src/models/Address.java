01e0e0bc61308b514437c888917c3b41aa5c4d06
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-23 23:08:10 +0430

    Add Address model

--- src/models/Address.java
+++ src/models/Address.java
@@ -1,2 +1,27 @@
-package models;public class Address {
+package models;
+
+public class Address {
+    private String name;
+    private String address;
+
+    public Address(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
