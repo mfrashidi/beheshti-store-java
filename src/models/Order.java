e4bb38c7f2d598bbc8fa507be292ade79f7b3845
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-23 23:08:57 +0430

    Add Order model

--- src/models/Order.java
+++ src/models/Order.java
@@ -1,2 +1,42 @@
-package models;public class Order {
+package models;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Order {
+    private Date orderTime;
+    private ArrayList<Product> products;
+    private int totalPrice;
+
+    public Order(ArrayList<String> products, int totalPrice) {
+        this.orderTime = new Date();
+        this.products = new ArrayList<>();
+        for (String id : products) this.products.add(Product.getProductByID(id));
+        this.totalPrice = totalPrice;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    public Date getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Date orderTime) {
+        this.orderTime = orderTime;
+    }
+
 }
