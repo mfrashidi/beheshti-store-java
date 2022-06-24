7a437cdbe22f5b194ed1b3cb67d2bda5aa7e4f92
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-24 11:17:43 +0430

    Add products to the database

--- src/database/DBHandler.java
+++ src/database/DBHandler.java
@@ -1,5 +1,6 @@
 package database;
 
+import models.Product;
 import models.User;
 
 import java.io.File;
@@ -12,6 +13,7 @@ import java.util.Scanner;
 public class DBHandler {
     private final static String initPath = "";
     private final static String usersFile = initPath + "users.json";
+    private final static String productsFile = initPath + "products.json";
 
     private static void saveUsers(ArrayList<User> users) {
         try (FileWriter fileWriter = new FileWriter(usersFile)){
@@ -48,5 +50,42 @@ public class DBHandler {
         ArrayList<User> users = getUsers();
         users.remove(user);
         saveUsers(users);
+    }
+
+    private static void saveProducts(ArrayList<Product> products) {
+        try (FileWriter fileWriter = new FileWriter(productsFile)){
+            for (Product product : products)
+                fileWriter.write(product.toJson() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Product> getProducts() {
+        ArrayList<Product> products = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(productsFile))){
+            while (scanner.hasNextLine())
+                products.add(Product.fromJson(scanner.nextLine()));
+        } catch (FileNotFoundException e) {
+            try (FileWriter fileWriter = new FileWriter(productsFile)) {
+                fileWriter.write("");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return products;
+    }
+
+    public static void addProducts(Product product) {
+        ArrayList<Product> products = getProducts();
+        if (!products.contains(product))
+            products.add(product);
+        saveProducts(products);
+    }
+
+    public static void removeProduct(Product product) {
+        ArrayList<Product> products = getProducts();
+        products.remove(product);
+        saveProducts(products);
     }
 }
