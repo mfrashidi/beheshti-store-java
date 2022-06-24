c2699bd955930f9ef6c245def141c7a82e16bdc0
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-24 09:57:22 +0430

    Add database handler

--- src/database/DBHandler.java
+++ src/database/DBHandler.java
@@ -1,2 +1,52 @@
-package database;public class DBHandler {
+package database;
+
+import models.User;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DBHandler {
+    private final static String initPath = "";
+    private final static String usersFile = initPath + "users.json";
+
+    private static void saveUsers(ArrayList<User> users) {
+        try (FileWriter fileWriter = new FileWriter(usersFile)){
+            for (User user : users)
+                fileWriter.write(user.toJson() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<User> getUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(usersFile))){
+            while (scanner.hasNextLine())
+                users.add(User.fromJson(scanner.nextLine()));
+        } catch (FileNotFoundException e) {
+            try (FileWriter fileWriter = new FileWriter(usersFile)) {
+                fileWriter.write("");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return users;
+    }
+
+    public static void addUser(User user) {
+        ArrayList<User> users = getUsers();
+        if (!users.contains(user))
+            users.add(user);
+        saveUsers(users);
+    }
+
+    public static void removeUser(User user) {
+        ArrayList<User> users = getUsers();
+        users.remove(user);
+        saveUsers(users);
+    }
 }
